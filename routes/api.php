f0468b997d7221399f8d71a70f0fad817f98ebe4
Programmer Copy Paste <ghozal@gmail.com>
2021-01-15 21:58:18 +0700

    kurang use model

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\CompaniesController;
-// use App\Models\Companies;
+use App\Models\Companies;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
